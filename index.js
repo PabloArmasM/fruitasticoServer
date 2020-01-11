a7f3d83298b9a8b4f6a199df5373ae715ca821fc
Pablo Armas Martín <Pabloarmasm@gmail.com>
2020-01-11 10:14:31 +0000

    Firebase send fruitdata

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 const express = require('express');
-const otro = require('./otro')
+const otro = require('./otro');
 const PORT = 3000;
 
 const app = express();
 
 
 var admin = require("firebase-admin");
-
 var serviceAccount = require("./fruitastico-6db6e-firebase-adminsdk-tyf3d-2e7b807e64.json");
 
-admin.initializeApp({
+const functions = require('firebase-functions');
+
+
+
+/*admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://fruitastico-6db6e.firebaseio.com"
-});
+});*/
+
+admin.initializeApp(
+  {credential: admin.credential.cert(serviceAccount)}
+);
+
+let db = admin.firestore();
 
 
 
-app.get('/', function(req, res){
-
+//HTTP server
+app.get('/getFruitCollection', function(req, res){
+  db.collection('fruit').get()
+  .then(snap => {
+    var JSONdata = {};
+    for (var i = 0; i < snap.size; i++) {
+      JSONdata[snap.docs[i].id] = snap.docs[i].data();
+    }
+    res.send(JSON.stringify(JSONdata));
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+    res.send("A ocurrido un error a la hora de obtener los documentos");
+  });
 });
 
 app.use('/source/css',express.static(__dirname +'/source/css'));
